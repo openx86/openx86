afdb43086df14f68af8e346f2c527241fe93a8bf
Chang Wei <changwei1006@gmail.com>
2021-12-03 04:07:42 +0800

    add rom for simulate on modelsim

---
+++ rtl/sim/memory.sv
@@ -0,0 +1,23 @@
+`timescale 1ns/1ns
+module memory #(
+    // parameters
+) (
+    // ports
+    input  logic [31:0] read_address,
+    output logic [31:0] read_data,
+    input  logic        clock,
+    input  logic        reset
+);
+
+reg [31:0] mem[0:31];  // 声明有32个32位的存储单元
+reg [31:0] n;
+
+assign read_data = mem[read_address];
+
+initial begin
+	$readmemb("D:\\GitHub\\openx86\\w80386dx\\rtl\\sim\\rom.bin", mem);
+    for(n=0;n<=31;n=n+1)
+        $display("mem[0x%h] = %h", n, mem[n]);
+end
+
+endmodule
