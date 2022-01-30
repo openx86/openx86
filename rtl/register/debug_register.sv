f4c0e7c069f695cffa0a85f4029321ba81044137
Chang Wei <changwei1006@gmail.com>
2022-01-31 04:20:48 +0800

    update debug_register, use array for output port

--- rtl/register/debug_register.sv
+++ rtl/register/debug_register.sv
@@ -16,43 +16,25 @@ module debug_register (
     input  logic         write_enable,
     input  logic [ 2: 0] write_index,
     input  logic [31: 0] write_data,
-    output logic [31: 0] DR0,
-    output logic [31: 0] DR1,
-    output logic [31: 0] DR2,
-    output logic [31: 0] DR3,
-    output logic [31: 0] DR4,
-    output logic [31: 0] DR5,
-    output logic [31: 0] DR6,
-    output logic [31: 0] DR7,
+    output logic [31: 0] DR [0:7],
     input  logic         clock, reset
 );
 
-reg [31:0] debug_reg [7:0];
-
 always_ff @( posedge clock or posedge reset ) begin
     if (reset) begin
-        debug_reg[0] <= 32'b0;
-        debug_reg[1] <= 32'b0;
-        debug_reg[2] <= 32'b0;
-        debug_reg[3] <= 32'b0;
-        debug_reg[4] <= 32'b0;
-        debug_reg[5] <= 32'b0;
-        debug_reg[6] <= 32'b0;
-        debug_reg[7] <= 32'b0;
+        DR[0] <= 32'b0;
+        DR[1] <= 32'b0;
+        DR[2] <= 32'b0;
+        DR[3] <= 32'b0;
+        DR[4] <= 32'b0;
+        DR[5] <= 32'b0;
+        DR[6] <= 32'b0;
+        DR[7] <= 32'b0;
     end else begin
         if (write_enable) begin
-            debug_reg[write_index] <= write_data;
+            DR[write_index] <= write_data;
         end
     end
 end
-
-assign DR0 = debug_reg[0];
-assign DR1 = debug_reg[1];
-assign DR2 = debug_reg[2];
-assign DR3 = debug_reg[3];
-assign DR4 = debug_reg[4];
-assign DR5 = debug_reg[5];
-assign DR6 = debug_reg[6];
-assign DR7 = debug_reg[7];
 
 endmodule
