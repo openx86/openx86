29e37bb0dfe310c282e482ca21ae26b7f71e6162
changwei <changwei1006@gmail.com>
2026-04-15 21:52:49 +0800

    feat: rename project files and update top-level entity for x86 SoC integration

--- fpga/boards/de2_115/x86_soc_on_de2_115.v
+++ fpga/boards/de2_115/x86_soc_on_de2_115.v
@@ -1,52 +1,9 @@
-// ============================================================================
-// Copyright (c) 2012 by Terasic Technologies Inc.
-// ============================================================================
-//
-// Permission:
-//
-//   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altera Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
-//   ,duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-//   This VHDL/Verilog or C/C++ source code is intended as a design reference
-//   which illustrates how these types of functions can be implemented.
-//   It is the user's responsibility to verify their design for
-//   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
-//   or functionality of this code.
-//
-// ============================================================================
-//           
-//  Terasic Technologies Inc
-//  9F., No.176, Sec.2, Gongdao 5th Rd, East Dist, Hsinchu City, 30070. Taiwan
-//
-//
-//
-//                     web: http://www.terasic.com/
-//                     email: support@terasic.com
-//
-// ============================================================================
-//
-// Major Functions:	DE2_115_Default
-//
-// ============================================================================
-//
-// Revision History :
-// ============================================================================
-//   Ver  :| Author              :| Mod. Date :| Changes Made:
-//   V1.1 :| HdHuang             :| 05/12/10  :| Initial Revision
-//   V2.0 :| Eko       				:| 05/23/12  :| version 11.1
-// ============================================================================
-
-module DE2_115_Default(
+module x86_soc_on_de2_115(
 
 	//////// CLOCK //////////
 	CLOCK_50,
-   CLOCK2_50,
-   CLOCK3_50,
+    CLOCK2_50,
+    CLOCK3_50,
 	ENETCLK_25,
 
 	//////// Sma //////////
