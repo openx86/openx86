7b5d065aa6a1fc6dc4e9143fbba2ea803642d43c
changwei <changwei1006@gmail.com>
2026-04-22 09:09:47 +0800

    refactor: remove redundant SYSTEMVERILOG_FILE assignments and clean up add_sv_files procedure

--- fpga/boards/de2_115/tcls/add_sv.tcl
+++ fpga/boards/de2_115/tcls/add_sv.tcl
@@ -1,12 +1,10 @@
+# Remove all SYSTEMVERILOG_FILE assignments
+remove_all_global_assignments -name SYSTEMVERILOG_FILE
+
 proc add_sv_files {dir} {
     foreach file [glob -nocomplain -directory $dir *.sv] {
         set_global_assignment -name SYSTEMVERILOG_FILE $file
     }
-
-    # # add .svh
-    # foreach file [glob -nocomplain -directory $dir *.svh] {
-    #     set_global_assignment -name VERILOG_INCLUDE_FILE $file
-    # }
 
     foreach subdir [glob -nocomplain -type d -directory $dir *] {
         add_sv_files $subdir
